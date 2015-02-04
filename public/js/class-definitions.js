014ddaa0edf2f6d0b4700f20fa451d75a8654a3b
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-02-03 19:39:52 -1000

    step 83

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1160,7 +1160,28 @@ Vehicle.prototype.drive = function(streetName) {
  * Any other number => "Could not determine type"
  *
  */
-
+Shape.prototype.getType = function() {
+  switch (this.sides) {
+    case 3:
+    return "triangle";
+    case 4:
+    return "quadrilateral";
+    case 5:
+    return "pentagon";
+    case 6:
+    return "hexagon";
+    case 7:
+    return "heptagon";
+    case 8:
+    return "octagon";
+    case 9:
+    return "nonagon";
+    case 10:
+    return "decagon";
+    default:
+    return "Could not determine type";
+  }
+}
 
 /* Step 84
  *
