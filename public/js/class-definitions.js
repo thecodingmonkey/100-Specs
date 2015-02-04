0d3a6321c836d7d1e3637711ffbffb70518c6e92
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-02-03 19:23:39 -1000

    step 73

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1080,7 +1080,7 @@ var dressShoes = new Shoe(10, "black");
 
 // Create 2 houses
 var singleStory = new House(1);
-var twoStory;
+var twoStory = new House(2);
 
 // Create 2 lightbulbs
 var incandescent;
