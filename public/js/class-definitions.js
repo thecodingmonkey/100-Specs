a2e201a3088e65d5bb8640c8117e089aa60ef83c
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-02-03 16:32:10 -1000

    step 12

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -142,6 +142,15 @@ var domains = {
  *
  */
 
+var browsers = {
+  Chromium: "google.com",
+  Safari: "apple.com",
+  Opera: "opera.com",
+  Firefox: "mozilla.org",
+  Sleipnir: "fenrir-inc.com",
+  Konqueror: "konqueror.org"
+};
+
 
 /* Step 13
  *
