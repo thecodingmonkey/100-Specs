404bdcec310fb6ce4c69262af8fb5282a4839244
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-02-03 20:03:11 -1000

    step 97

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1371,8 +1371,8 @@ var blackShoesSize10 = new Shoe(10, 'black').findShoes();
  * and assign the values to each variable below.
  *
  */
-var farTooTallAStory;
-var shortStory;
+var farTooTallAStory = new House(2).isATallStory(1);
+var shortStory = new House(2).isATallStory(3);
 
 
  /* Step 98
