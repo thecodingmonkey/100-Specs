f44bbf28ff5d272d11ce53196035fdba9f604430
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-02-03 19:23:25 -1000

    step 72

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1079,7 +1079,7 @@ var rubySlippers = new Shoe(7, "red");
 var dressShoes = new Shoe(10, "black");
 
 // Create 2 houses
-var singleStory;
+var singleStory = new House(1);
 var twoStory;
 
 // Create 2 lightbulbs
