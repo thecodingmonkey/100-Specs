31144b71d54658ff9012bb572aaaa3f8ccce4135
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-02-03 16:34:49 -1000

    step 13

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -168,7 +168,18 @@ var browsers = {
  *  isDouble  => true
  *
  */
-
+var rainbow = {
+  colors: {
+    red :   "#F0280A",
+    orange: "#FF8800",
+    yellow: "#FFDD00",
+    green:  "#51AB0C",
+    blue:   "#1593ED",
+    indigo: "#5215ED",
+    violet: "#A915ED"
+  },
+  isDouble: true
+};
 
 /* Step 14
  *
