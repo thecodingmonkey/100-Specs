27daadca82cee47f0d00079ab2c6ccc079e4bafd
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-02-03 18:50:31 -1000

    step 33

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -634,7 +634,27 @@ SolarSystem.prototype.removePlanet = function(planet) {
  *   marries
  *
  */
-
+function PrincessLeia(name, money, age, gender) {
+  Person.call(this, name, money, age, gender);
+  this.isInTrouble = null;
+}
+PrincessLeia.prototype = Object.create(Person.prototype, { 
+  constructor: { value: Person } 
+});
+PrincessLeia.prototype.shootsGun = function() {
+  this.isInTrouble = false;
+  return "Leia shoots her gun wildly";
+};
+PrincessLeia.prototype.getsInTrouble = function() {
+  this.isInTrouble = true;
+  return "Help me Obi-wan Kenobi, you're my only hope";  
+};
+PrincessLeia.prototype.marries = function(loveInterest) {
+  if (loveInterest === "Luke Skywalker") {
+    return "Gross!";
+  }
+  return ("Han Solo" === loveInterest);
+};
 
 /* Step 34
  *
